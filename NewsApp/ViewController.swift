bb7068f18215242c45682d70f33bdcf1c0aac356
海法修平 <fnmmbestsk15kkpo@gmail.com>
2019-08-08 21:29:32 +0900

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by 海法修平 on 2019/08/08.
+//  Copyright © 2019 shu26. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
